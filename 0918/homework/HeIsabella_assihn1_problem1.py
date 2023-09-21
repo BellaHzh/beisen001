fb893e2abc55ed0345c9e4196f1def244eb824ee
bellaHzh <isabellahe2021@gmail.com>
2023-09-21 12:58:34 +0800

    add HeIsabella_assign1_problem1

---
+++ 0918/homework/HeIsabella_assihn1_problem1.py
@@ -0,0 +1,5 @@
+# Randomly select two numbers from the list, and compare them, and place the smaller one infront of the larger one.
+# Compare a new number with these two individually, and place the smaller one infront of the larger one.
+# For example, firstly compare 2 and 4; compare 3 with 2 and 4 respectively. Make the list: 2,3,4
+# Follow this step to make a list of numbers from small to large.
+# The last one is the largest number on the list.
