904c647968fe94981ce3b77c5dacf0316a6ece78
bellaHzh <isabellahe2021@gmail.com>
2023-09-22 19:19:53 +0800

    add homework

---
+++ 0918/homework/HeIsabella_assign1_problem2.py
@@ -0,0 +1,4 @@
+print("Each player choose to use either 'X'or'O'")
+print("'X'or'O' cannot be changed after chosen, and the two players cannot choose the same signal")
+print("Two players have the option to choose one grid within 3*3=9 gridlines")
+print("The first player to form a horizontal, vertical, or diagonal line with three consecutive symbols wins!")
