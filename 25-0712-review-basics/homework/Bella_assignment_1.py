add845b4b82a6d6be3ea4a770761f60a51c03bd3
Bella <macbookpro@Bellas-MacBook-Air.local>
2025-07-12 21:54:33 -0400

    Initial commit: Add Bella_assignment_1.py

--- 25-0712-review-basics/homework/Bella_assignment_1.py
+++ 25-0712-review-basics/homework/Bella_assignment_1.py
@@ -40,3 +40,17 @@ print(f"{'2':>5}{amount_one_month_new:>20}{interest_two_month_new:>15}{amount_tw
 print(f"{'3':>5}{amount_two_month_new:>20}{interest_three_month_new:>15}{amount_three_month_new:>20}")
 print(f"{'4':>5}{amount_three_month_new:>20}{interest_four_month_new:>15}{amount_four_month_new:>20}")
 '''
+# Part C
+Fahrenheit = int(input("Enter a temperature in degrees Fahrenheit: "))
+print()
+degree_sign = u'\N{DEGREE SIGN}'
+print(str(Fahrenheit)+degree_sign, "F ...")
+print()
+Fahrenheit_new = float(Fahrenheit)
+Celsius = format((Fahrenheit_new-32)*5/9, ".1f")
+print(f"{'... in Celsius':<15}{Celsius:>20}{degree_sign:>1}", "C")
+Celsius_new = float(Celsius)
+Kelvin = format(Celsius_new+273.15, ".1f")
+Rankine = float(format(Fahrenheit_new+459.67, ".1f"))
+print(f"{'... in Kelvin':<15}{Kelvin:>20}{degree_sign:>1}", "K")
+print(f"{'... in Rankine':<15}{Rankine:>20}{degree_sign:>1}", "R")
