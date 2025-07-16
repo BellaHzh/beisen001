13f24793b77d544a93803d2559f5940b20a67f8f
BellaHzh <isabellahe2021@gmail.com>
2025-07-16 18:04:49 -0400

    bella_homework2_final_version

--- 25-0713-bool-and-cond/homework/Bella_assignment_2.py
+++ 25-0713-bool-and-cond/homework/Bella_assignment_2.py
@@ -1,5 +1,6 @@
 
 # Part A
+import random
 print("NYC Screen Printing")
 print()
 number_of_shirts = int(
@@ -85,3 +86,57 @@ else:
             print("You get Mimo! The old man can never know that you are a couple. Ahah!")
         else:
             print("Mimo runs away. He is so angry. How can you let him go! Get him back!")
+# Part C
+print("I'm thinking of a number between 1 and 10. You have three tries to guess it.")
+print()
+random_number = random.randint(1, 10)
+guess_1 = int(input("Guess my number: "))
+if guess_1 == random_number:
+    print("You got it!")
+    print()
+elif guess_1 < random_number:
+    print("Too low, try again.")
+    print()
+    guess_2 = int(input("Guess my number: "))
+    if guess_2 == random_number:
+        print("You got it!")
+    elif guess_2 < random_number:
+        print("Too low, try again.")
+        print()
+        guess_3 = int(input("Guess my number: "))
+        if guess_3 == random_number:
+            print("You got it!")
+        else:
+            print("Sorry, game over.")
+    elif guess_2 > random_number:
+        print("Too high, try again.")
+        print()
+        guess_3 = int(input("Guess my number: "))
+        if guess_3 == random_number:
+            print("You got it!")
+        else:
+            print("Sorry, game over.")
+elif guess_1 > random_number:
+    print("Too high, try again.")
+    print()
+    guess_2 = int(input("Guess my number: "))
+    if guess_2 == random_number:
+        print("You got it!")
+    elif guess_2 < random_number:
+        print("Too low, try again.")
+        print()
+        guess_3 = int(input("Guess my number: "))
+        if guess_3 == random_number:
+            print("You got it!")
+        else:
+            print("Sorry, game over.")
+    elif guess_2 > random_number:
+        print("Too high, try again.")
+        print()
+        guess_3 = int(input("Guess my number: "))
+        if guess_3 == random_number:
+            print("You got it!")
+        else:
+            print("Sorry, game over.")
+print()
+print(f"The secret number was {random_number}.")
