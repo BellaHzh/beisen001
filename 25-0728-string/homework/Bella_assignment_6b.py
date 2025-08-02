cd5ee443367e1ab1e57921e9bb5fd1c56d5e4c9a
jsl1114 <ljs11141118@gmail.com>
2025-08-01 22:41:47 -0400

    grade hw 6

--- 25-0728-string/homework/Bella_assignment_6b.py
+++ 25-0728-string/homework/Bella_assignment_6b.py
@@ -1,5 +1,4 @@
 import random
-import string
 
 # Function to Add Letters
 
@@ -8,9 +7,9 @@ def add_letters(word, num):
     new_word = ""
     for c in word:
         added_letters = ""
-        for i in range(num):
+        for _ in range(num):
             random_number_1 = random.randint(65, 90)
-            random_number_2 = random.randint(65, 90)
+            random_number_2 = random.randint(97, 122)
             random_number = random.choice([random_number_1, random_number_2])
             random_letter = chr(random_number)
             added_letters += random_letter
@@ -46,13 +45,14 @@ def main():
         choice = input("(e)ncode, (d)ecode or (q)uit: ")
         if choice == "q":
             break
-        while choice not in ["e", "d", "q"]:
+        while choice not in "edq":
             choice = input("(e)ncode, (d)ecode or (q)uit: ")
 
         num = int(input("Enter a number between 1 and 5: "))
         while num <= 0 or num > 5:
             num = int(input("Enter a number between 1 and 5: "))
-        word = input("Enter a phrase to encode: ")
+        word = input(
+            f"Enter a phrase to {'encode' if choice == 'e' else 'decode'}: ")
         if choice == "e":
             word_added = add_letters(word, num)
             word_encoded = shift_characters(word_added, num)
